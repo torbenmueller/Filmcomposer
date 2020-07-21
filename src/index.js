398005b1d5bb48156e612f315697c643e91d9bfc
Torben Müller <torbenmuller@Torbens-iMac.local>
2020-07-22 00:19:30 +0200

    Backup1

--- src/index.js
+++ src/index.js
@@ -1,17 +1,75 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+import { Home } from './components/Home';
+import { About } from './components/About';
+import { Contact } from './components/Contact';
+import { NoMatch } from './components/NoMatch';
+import { Impressum } from './components/Impressum';
+import { Datenschutzerklaerung } from './components/Datenschutzerklaerung';
+import { Layout } from './components/Layout';
+import { Jumbotron } from './components/Jumbotron';
+import Footer from './components/Footer';
+import './fontawesome';
+import { Navbar, Nav } from 'react-bootstrap';
+import { withRouter } from 'react-router';
+import logo from './assets/tjm_logo.svg';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const Header = props => {
+  const { location } = props;
+  return (
+    <Navbar collapseOnSelect expand="lg" variant="dark" className="navbar-bgcolor">
+      <Navbar.Brand href="/">
+      <img
+        src={logo}
+        width="30"
+        height="30"
+        className="d-inline-block align-top"
+        alt="React Bootstrap logo"
+      />
+    </Navbar.Brand>
+      <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+      <Navbar.Collapse id="responsive-navbar-nav">
+      <Nav activeKey={location.pathname} className="mr-auto">
+          <Nav.Link href="/">Home</Nav.Link>
+          <Nav.Link href="/about">About</Nav.Link>
+          <Nav.Link href="/contact">Contact</Nav.Link>
+      </Nav>
+      <Nav activeKey={location.pathname}>
+          <Nav.Link href="/impressum">Impressum</Nav.Link>
+          <Nav.Link href="/datenschutzerklaerung">Datenschutzerklärung</Nav.Link>
+      </Nav>
+      </Navbar.Collapse>
+  </Navbar>
+  );
+};
+const HeaderWithRouter = withRouter(Header);
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+class MyHeader extends React.Component {
+
+  render() {
+    return (
+      <React.Fragment>
+        <Router>
+        <HeaderWithRouter />
+        <Jumbotron />
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Layout>
+              <Route path="/about" component={About} />
+              <Route path="/contact" component={Contact} />
+              <Route path="/impressum" component={Impressum} />
+              <Route path="/datenschutzerklaerung" component={Datenschutzerklaerung} />
+            </Layout>
+            <Route component={NoMatch} />
+          </Switch>
+        </Router>
+        <Footer />
+      </React.Fragment>
+    );
+  }
+}
+
+ReactDOM.render(<MyHeader />, document.getElementById('root'));
